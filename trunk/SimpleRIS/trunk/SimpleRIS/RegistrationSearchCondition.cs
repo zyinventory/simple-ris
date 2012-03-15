c734f05b13dbc6c1850a141b09471e2ecf7d3be4
zyinventory@gmail.com <zyinventory@gmail.com@d87e6619-9b79-1822-deb7-a4dd28420a08>
2012-03-15 04:06:32 +0000

    import

---
+++ trunk/SimpleRIS/trunk/SimpleRIS/RegistrationSearchCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SimpleRIS
+{
+    public partial class RegistrationSearchCondition : DevExpress.XtraEditors.XtraUserControl
+    {
+        public RegistrationSearchCondition()
+        {
+            InitializeComponent();
+        }
+    }
+}
