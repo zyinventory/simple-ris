03e5e36e61b8829d70193dabc6314528faa726de
zyinventory@gmail.com <zyinventory@gmail.com@d87e6619-9b79-1822-deb7-a4dd28420a08>
2013-04-10 11:53:58 +0000

    删除ofpacs.h

--- dcmtk-3.5.4/ofstd/include/dcmtk/ofstd/ofpacs.h
+++
@@ -1,3 +0,0 @@
-OFCondition MkdirRecursive(OFString& subdirectoryPath);
-int GenerateLogPath(char *buf, size_t bufLen, const char *appName, const char pathSeparator);
-BOOL DeleteEmptyFile(const char *filePath);
