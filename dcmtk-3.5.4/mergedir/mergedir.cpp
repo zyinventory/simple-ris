1b511524bed8e76dd6bc39915f8e68becc5b4431
zyinventory@gmail.com <zyinventory@gmail.com@d87e6619-9b79-1822-deb7-a4dd28420a08>
2013-05-04 10:02:16 +0000

    test traversal of dicomdir

--- dcmtk-3.5.4/mergedir/mergedir.cpp
+++ dcmtk-3.5.4/mergedir/mergedir.cpp
@@ -24,6 +24,9 @@ static char rcsid[] = "$dcmtk: " OFFIS_CONSOLE_APPLICATION " v"
 int main(int argc, char *argv[])
 {
     const char *opt_output = DEFAULT_DICOMDIR_NAME;
+	const char *opt_fileset = DEFAULT_FILESETID;
+    const char *opt_descriptor = NULL;
+    const char *opt_charset = DEFAULT_DESCRIPTOR_CHARSET;
 
 	SetDebugLevel(( 0 ));
 	OFConsoleApplication app(OFFIS_CONSOLE_APPLICATION, OFFIS_CONSOLE_DESCRIPTION, rcsid);
@@ -33,8 +36,14 @@ int main(int argc, char *argv[])
 
 	cmd.addParam("dicomdir-in", "referenced DICOMDIR file", OFCmdParam::PM_MultiOptional);
 	cmd.addGroup("input options:");
-	cmd.addOption("--output-file",           "-o", 1,  "[f]ilename : string",
+	cmd.addOption("--output-file",           "+D", 1,  "[f]ilename : string",
                                                            "generate specific DICOMDIR file\n(default: " DEFAULT_DICOMDIR_NAME " in current directory)");
+    cmd.addOption("--fileset-id",            "+F", 1,  "[i]d : string (default: " DEFAULT_FILESETID ")",
+                                                       "use specific file set ID");
+    cmd.addOption("--descriptor",            "+R", 1,  "[f]ilename : string",
+                                                       "add a file set descriptor file ID\n(e.g. README, default: no descriptor)");
+    cmd.addOption("--char-set",              "+C", 1,  "[c]har-set : string",
+                                                           "add a specific character set for descriptor\n(default: \"" DEFAULT_DESCRIPTOR_CHARSET "\" if descriptor present)");
 
 	/* evaluate command line */
     prepareCmdLineArgs(argc, argv, OFFIS_CONSOLE_APPLICATION);
@@ -45,14 +54,20 @@ int main(int argc, char *argv[])
             app.printUsage();
         if (cmd.findOption("--output-file"))
             app.checkValue(cmd.getValue(opt_output));
+        if (cmd.findOption("--fileset-id"))
+            app.checkValue(cmd.getValue(opt_fileset));
+        if (cmd.findOption("--descriptor"))
+            app.checkValue(cmd.getValue(opt_descriptor));
+        if (cmd.findOption("--char-set"))
+            app.checkValue(cmd.getValue(opt_charset));
 	}
 
     OFList<OFString> fileNames;
     const char *param = NULL;
-    const int count = cmd.getParamCount();
 
-    if (count == 0) app.printUsage();
-	for (int i = 1; i <= count; i++)
+	const int paramCount = cmd.getParamCount();
+    if (paramCount == 0) app.printUsage();
+	for (int i = 1; i <= paramCount; i++)
 	{
 		cmd.getParam(i, param);
 		fileNames.push_back(param);
@@ -71,23 +86,65 @@ int main(int argc, char *argv[])
     }
 
 	OFCondition result;
-	DcmDicomDir *DicomDir = new DcmDicomDir(fileNames.front().c_str());
-	if (DicomDir != NULL)
-		result = DicomDir->error();
+	DcmDicomDir *output = new DcmDicomDir(opt_output, opt_fileset);
+	if (output != NULL)
+		result = output->error();
 	else
-		result = EC_MemoryExhausted;
-	DcmDirectoryRecord *rootRecord = &(DicomDir->getRootRecord());
-	DcmDirectoryRecord *recPatient = rootRecord->nextSub(NULL);
-	if(recPatient && recPatient->getRecordType() == ERT_Patient)
+		{ CERR << "create or open output file " << opt_output << " error" << endl; exit(1); }
+	DcmDirectoryRecord *outputRoot = &(output->getRootRecord());
+
+	OFListIterator(OFString) curr = fileNames.begin();
+	for(; curr != fileNames.end(); ++curr)
 	{
-		DcmElement *patientId = NULL;
-		recPatient->findAndGetElement(DCM_PatientID, patientId);
-		if(patientId)
+		DcmDicomDir *inputDir = new DcmDicomDir((*curr).c_str());
+		if (inputDir != NULL)
+			result = inputDir->error();
+		else
+			{ CERR << "open input file " << *curr << " error" << endl; continue; }
+		COUT << "open input file " << *curr << endl;
+		DcmDirectoryRecord *rootRecord = &(inputDir->getRootRecord());
+		DcmStack resultStack;
+
+		// find all image
+		DcmUniqueIdentifier *ptrInstanceUID = NULL;
+		resultStack.push(rootRecord);
+		while(rootRecord->search(DCM_ReferencedSOPInstanceUIDInFile, resultStack, ESM_afterStackTop).good())
 		{
-			OFString pid;
-			patientId->getOFString(pid, 0);
-			COUT << pid << endl;
+			DcmObject * object = resultStack.top();
+			if(object && (ptrInstanceUID = OFdynamic_cast(DcmUniqueIdentifier*, object)))
+			{
+				OFString uid;
+				result = ptrInstanceUID->getOFString(uid, 0);
+				if(result.good())
+				{
+					COUT << uid << endl;
+				}
+			}
 		}
+
+		// find patientId in patient
+		if(DcmDirectoryRecord *patient = rootRecord->nextSub(NULL))
+		{
+			resultStack.clear();
+			resultStack.push(patient);
+			while(patient->search(DCM_PatientID, resultStack, ESM_afterStackTop).good())
+			{
+				DcmObject * object = resultStack.top();
+				DcmLongString *patientId = NULL;
+				if(object && (patientId = OFdynamic_cast(DcmLongString*, object)))
+				{
+					OFString uid;
+					result = patientId->getOFString(uid, 0);
+					if(result.good())
+					{
+						COUT << uid << endl;
+					}
+				}
+			}
+		}
+		delete inputDir;
 	}
-	delete DicomDir;
+	//outputRoot->print(COUT);
+	//output->write(EXS_LittleEndianExplicit, EET_ExplicitLength, EGL_recalcGL);
+	delete output;
 }
