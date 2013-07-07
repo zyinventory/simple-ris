a9127e5aff02fc0a8d257ccdf31b28c943626466
zyinventory@gmail.com <zyinventory@gmail.com@d87e6619-9b79-1822-deb7-a4dd28420a08>
2013-07-07 13:46:19 +0000

    mergedir: 1st release.

--- trunk/dcmtk-3.5.4/mergedir/mergedir.cpp
+++ trunk/dcmtk-3.5.4/mergedir/mergedir.cpp
@@ -21,6 +21,10 @@ static char rcsid[] = "$dcmtk: " OFFIS_CONSOLE_APPLICATION " v"
 #define SHORTCOL 4
 #define LONGCOL 23
 
+OFBool	opt_verbose = OFFalse;
+
+OFCondition mergeToDest(DcmDirectoryRecord *dest, DcmDirectoryRecord *src);
+
 int main(int argc, char *argv[])
 {
     const char *opt_output = DEFAULT_DICOMDIR_NAME;
@@ -36,6 +40,7 @@ int main(int argc, char *argv[])
 
 	cmd.addParam("dicomdir-in", "referenced DICOMDIR file", OFCmdParam::PM_MultiOptional);
 	cmd.addGroup("input options:");
+	cmd.addOption("--verbose",               "-v",     "verbose mode, print processing details");
 	cmd.addOption("--output-file",           "+D", 1,  "[f]ilename : string",
                                                            "generate specific DICOMDIR file\n(default: " DEFAULT_DICOMDIR_NAME " in current directory)");
     cmd.addOption("--fileset-id",            "+F", 1,  "[i]d : string (default: " DEFAULT_FILESETID ")",
@@ -49,6 +54,7 @@ int main(int argc, char *argv[])
     prepareCmdLineArgs(argc, argv, OFFIS_CONSOLE_APPLICATION);
     if (app.parseCommandLine(cmd, argc, argv, OFCommandLine::ExpandWildcards))
     {
+		if (cmd.findOption("--verbose")) opt_verbose=OFTrue;
         /* print help text and exit */
         if (cmd.getArgCount() == 0)
             app.printUsage();
@@ -85,23 +91,23 @@ int main(int argc, char *argv[])
         return 1;  /* DcmDicomDir class dumps core when no data dictionary */
     }
 
-	OFCondition result;
-	DcmDicomDir *output = new DcmDicomDir(opt_output, opt_fileset);
-	if (output != NULL)
-		result = output->error();
+	OFCondition cond;
+	DcmDicomDir *dest = new DcmDicomDir(opt_output, opt_fileset);
+	if (dest != NULL)
+		cond = dest->error();
 	else
 		{ CERR << "create or open output file " << opt_output << " error" << endl; exit(1); }
-	DcmDirectoryRecord *outputRoot = &(output->getRootRecord());
+	DcmDirectoryRecord *destRoot = &(dest->getRootRecord());
 
 	for(OFListIterator(OFString) curr = fileNames.begin(); curr != fileNames.end(); ++curr)
 	{
-		DcmDicomDir *inputDir = new DcmDicomDir((*curr).c_str());
-		if (inputDir != NULL)
-			result = inputDir->error();
+		DcmDicomDir *src = new DcmDicomDir((*curr).c_str());
+		if (src != NULL)
+			cond = src->error();
 		else
 			{ CERR << "open input file " << *curr << " error" << endl; continue; }
 		COUT << "open input file " << *curr << endl;
-		DcmDirectoryRecord *rootRecord = &(inputDir->getRootRecord());
+		DcmDirectoryRecord *srcRoot = &(src->getRootRecord());
 		DcmStack resultStack;
 
 		// find all image
@@ -123,28 +129,129 @@ int main(int argc, char *argv[])
 		}
 		*/
 		// find patientId in patient
-		if(DcmDirectoryRecord *patient = rootRecord->nextSub(NULL))
+		resultStack.clear();
+		DcmDirectoryRecord *patient = NULL;
+		while(patient = srcRoot->nextSub(patient))
 		{
-			resultStack.clear();
-			resultStack.push(patient);
-			while(patient->search(DCM_PatientID, resultStack, ESM_afterStackTop).good())
+			cond = mergeToDest(destRoot, patient);
+		}
+		delete src;
+	}
+	dest->write(EXS_LittleEndianExplicit, EET_ExplicitLength, EGL_recalcGL);
+	delete dest;
+}
+
+OFCondition mergeToDest(DcmDirectoryRecord *dest, DcmDirectoryRecord *src)
+{
+	DcmTagKey key, subKey;
+	E_DirRecType srcType, upperType;
+	if(src == NULL || dest == NULL) return EC_IllegalParameter;
+	srcType = src->getRecordType();
+	switch(srcType)
+	{
+	case ERT_Patient:
+		key = DCM_PatientID;
+		subKey = DCM_StudyInstanceUID;
+		upperType = ERT_root;
+		if(opt_verbose) CERR << "merge patient level ..." << endl;
+		break;
+	case ERT_Study:
+		key = DCM_StudyInstanceUID;
+		subKey = DCM_SeriesInstanceUID;
+		upperType = ERT_Patient;
+		if(opt_verbose) CERR << "merge study level ..." << endl;
+		break;
+	case ERT_Series:
+		key = DCM_SeriesInstanceUID;
+		subKey = DCM_ReferencedSOPInstanceUIDInFile;
+		upperType = ERT_Study;
+		if(opt_verbose) CERR << "merge series level ..." << endl;
+		break;
+	case ERT_Image:
+		key = DCM_ReferencedSOPInstanceUIDInFile;
+		subKey = DCM_ReferencedSOPInstanceUIDInFile;
+		upperType = ERT_Series;
+		if(opt_verbose) CERR << "merge instance level ..." << endl;
+		break;
+	default:
+		CERR << "src's record type is unexpected:" << endl;
+		CERR << "src:" << endl;
+		src->print(CERR);
+		return EC_IllegalParameter;
+	}
+
+	if(upperType != dest->getRecordType())
+	{
+		CERR << "src's and dest's record type are mismatched:" << endl;
+		CERR << "dest:" << endl;
+		dest->print(CERR);
+		CERR << "src:" << endl;
+		src->print(CERR);
+		return EC_IllegalParameter;
+	}
+
+	OFString srcUid, destSubUid;
+	DcmElement *elementId = NULL, *destElementId = NULL;
+	if(EC_Normal == src->findAndGetElement(key, elementId) && 
+		elementId->getOFString(srcUid, 0).good())
+	{
+		DcmDirectoryRecord *destSub = NULL;
+		while(destSub = dest->nextSub(destSub))
+		{
+			if(destSub->getRecordType() != srcType)
 			{
-				DcmObject * object = resultStack.top();
-				DcmLongString *patientId = NULL;
-				if(object && (patientId = OFdynamic_cast(DcmLongString*, object)))
+				CERR << "src's and destSub's record type are mismatched:" << endl;
+				CERR << "src:" << endl;
+				src->print(CERR);
+				CERR << "destSub:" << endl;
+				destSub->print(CERR);
+				continue;
+			}
+
+			if(EC_Normal == destSub->findAndGetElement(key, destElementId) && 
+				destElementId->getOFString(destSubUid, 0).good())
+			{
+				if(srcUid == destSubUid)
 				{
-					OFString uid;
-					result = patientId->getOFString(uid, 0);
-					if(result.good())
+					if(srcType == ERT_Image)
 					{
-						COUT << uid << endl;
+						CERR << "skip same image " << srcUid << endl;
+						return EC_Normal;
 					}
+					DcmDirectoryRecord *srcSub = NULL;
+					while(srcSub = src->nextSub(srcSub))
+					{
+						if(mergeToDest(destSub, srcSub).bad())
+						{
+							CERR << "mergeToDest failed:" << endl;
+							CERR << "srcSub:" << endl;
+							srcSub->print(CERR);
+							CERR << "destSub:" << endl;
+							destSub->print(CERR);
+						}
+					}
+					return EC_Normal;
 				}
+				// else continue;
+			}
+			else
+			{
+				CERR << "Can not find " << key.toString() << " in destSub:" << endl;
+				destSub->print(CERR);
 			}
 		}
-		delete inputDir;
+
+		if(opt_verbose)
+		{
+			CERR << "insert new record:" << endl;
+			src->print(CERR);
+		}
+		return dest->insertSub(OFdynamic_cast(DcmDirectoryRecord*, src->clone()));
 	}
-	//outputRoot->print(COUT);
-	//output->write(EXS_LittleEndianExplicit, EET_ExplicitLength, EGL_recalcGL);
-	delete output;
+	else
+	{
+		CERR << "Can not find " << key.toString() << " in src:" << endl;
+		src->print(CERR);
+	}
+	return EC_IllegalParameter;
 }
