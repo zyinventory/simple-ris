d88bec6fb9b6f0664a0528f6e34dccdd5ee7f846
zyinventory@gmail.com <zyinventory@gmail.com@d87e6619-9b79-1822-deb7-a4dd28420a08>
2013-11-05 15:40:40 +0000

    base24 enc & dec OK

--- dcmtk-3.5.4/gencard/gencard.h
+++ dcmtk-3.5.4/gencard/gencard.h
@@ -1,6 +1,11 @@
 #pragma once
-
+				   //          11111111112222
+                   //012345678901234567890123
 const char code[] = "9TF7VMWB8XJ2CKRQ346YPDGH";
+const char decode[40] = {						   // 0x32
+      11,16,17,-1,18, 3, 8, 0,-1,-1,-1,-1,-1,-1,   //    2 3 4 5 6 7 8 9 _ _ _ _ _ _
+-1,-1, 7,12,21,-1, 2,22,23,-1,10,13,-1, 5,-1,-1,   //_ _ B C D _ F G H _ J K _ M _ _
+20,15,14,-1, 1,-1, 4, 6, 9,19 };                   //P Q R _ T _ V W X Y
 
 struct lock_key_st { unsigned int key[4]; unsigned int serial, lockNumber; };
 typedef struct lock_key_st LOCK_KEY;
